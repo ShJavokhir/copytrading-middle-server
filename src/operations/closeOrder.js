ea37909eeed00f0cce1c6027c5f1cd0538079602
Javokhir Shomuratov <javohirshomuradov@gmail.com>
2021-04-18 21:31:21 +0500

    removed deleteOrder and openPending order, added orderType field to closeOrder module

--- src/operations/closeOrder.js
+++ src/operations/closeOrder.js
@@ -1,11 +1,13 @@
 const closeOrder = (req, res, next, socket) => {
   const orderSymbol = req.body.orderSymbol;
   const orderTicket = req.body.orderTicket;
+  const orderType = req.body.orderType;
   const orderCloseTime = req.body.orderCloseTime;
 
   const responseJsonObject = {
     action: "orderClose",
     orderTicket: orderTicket,
+    orderType: orderType,
     orderSymbol: orderSymbol,
     orderCloseTime: orderCloseTime,
   };
